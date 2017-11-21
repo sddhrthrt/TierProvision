ed6b196f17e61df741bcd80779d27ce5786fdece
sddhrthrt@gmail.com <sddhrthrt@gmail.com>
2017-11-20 22:00:22 -0500

    destruction algorithm, logs

--- scheduler/algorithms.py
+++ scheduler/algorithms.py
@@ -1,6 +1,8 @@
 from src.schema import Session, Task, Node, TaskRequest, TaskStats
 from executor.nomad_api import nomadServer, NomadJob
+import logging
 
+logger = logging.getLogger(__name)
 
 class Algorithm():
 
@@ -8,12 +10,13 @@ class Algorithm():
     pass
 
 
-class DumbAlgorithm():
+class DumbAlgorithm(Algorithm):
 
   def process_requests(self):
     session = Session()
     requests = session.query(TaskRequest).all()
     for i in requests:
+      logger.debug("Processing request: %s", i)
       task_id = i.task_id
       task = session.query(Task).filter(id=task_id).first()
       # Query TaskStatus table and figure out if 
@@ -23,9 +26,23 @@ class DumbAlgorithm():
                              task.cpu, task.memory)
       if status:
         #TODO: COMPLEX LOGIC!!!! 
+        logger.debug("Updating job: %s", j)
         nomadServer.update_job(j)
       else:
+        logger.debug("Creating job: %s", j)
         nomadServer.register_job(j)
 
   def reprovision(self):
     return
+
+
+class DestructionAlgorithm(Algorithm):
+
+  def process_requests(self):
+    session = Session()
+    tasks = session.query(Task).all()
+    for task in tasks:
+      j = NomadJob(task.name, task.image,
+                             task.cpu, task.memory)
+      logger.debug("Deleting job: %s", j)
+      nomadServer.delete_job(j)
